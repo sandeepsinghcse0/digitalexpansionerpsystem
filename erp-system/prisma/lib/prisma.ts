5a70e00db299f93071f257ed18cf42ab61567af3
sandeep <sandeepsinghcse0@gmail.com>
2026-06-06 15:16:21 +0530

    Updated the database table model

--- erp-system/prisma/lib/prisma.ts
+++ erp-system/prisma/lib/prisma.ts
@@ -1,6 +1,23 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "../../generated/prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  connectionLimit: 5,
+});
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
 };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
